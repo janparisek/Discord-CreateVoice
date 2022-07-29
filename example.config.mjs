c8b7b24f2ea93e66ce01a52f020e80c927563e28
NKN1396 <someone.took.nkn1396@gmail.com>
2022-07-29 13:20:56 +0200

    Update config.mjs with correct configuration example

--- example.config.mjs
+++ example.config.mjs
@@ -1,22 +1,9 @@
 const config = [
   {
-    message: '446409003136712726',
-    channel: '414617785415630855',
-    disjoint: false,
-    reactions: [
-      {
-        emoji: '🤔',
-        roles: [
-          '446783613439311882'
-        ]
-      },
-      {
-        emoji: 'b26:441046394493599764',
-        roles: [
-          '446783667084197910'
-        ]
-      }
-    ]
+    guild: '446409003136712726',
+    category: '',
+    position: 3,
+    ttl: 120
   }
 ]
 
